9a12cf695ac6d1342f9b313e49a779881017e99a
jaimebp07 <andrescodigo07@gmail.com>
2025-04-16 10:17:30 -0500

    ✨ add an example with concatMap operator

--- RxJs/transformation-operators/07-concatMap.ts
+++ RxJs/transformation-operators/07-concatMap.ts
@@ -6,5 +6,5 @@ const interval$ = interval(1000).pipe(take(3));
 
 click$.pipe(
     map<PointerEvent, number>(val => val.x),
-    concatMap<number, Observable<string>>(val => interval$.pipe( map(i => `Position X: ${val}, interval: ${i} position x ${val}`)))// keeps only the subscription to the last observable
+    concatMap<number, Observable<string>>(val => interval$.pipe( map(i => `Position X: ${val}, interval: ${i} position x ${val}`)))// Maintains all the subscriptions and displays the emitted value for queued observables
 ).subscribe(val => console.log(`${val}`))
